79c7605d17dca136b2e5db0a60aa66ef8d9f6691
Magdalena Sarandeva <sarandeva2@gmail.com>
2022-06-10 14:51:44 +0200

    End chapter 15 | Smart Routes: POST & Validate {x}

--- cauldron_overflow5.4/src/Controller/CommentController.php
+++ cauldron_overflow5.4/src/Controller/CommentController.php
@@ -4,17 +4,16 @@ namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
-/**
- * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
- */
+    /**
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
+     */
     public function commentVote($id, $direction, LoggerInterface $logger)
     {
-        // todo - use id to query the database
+        // todo - use id to query the database: no protection needed, DB has for id's. oo 404 page
 
         // use real logic here to save this to the database
         if ($direction === 'up') {
