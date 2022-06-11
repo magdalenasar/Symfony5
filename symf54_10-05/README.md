e9e5af4548a6790ef7168d3784597eadc9775f97
Magdalena Sarandeva <sarandeva2@gmail.com>
2022-06-11 19:58:56 +0200

    Project opstellen, PHP versies testen | les di 10-05-2022

---
+++ symf54_10-05/README.md
@@ -0,0 +1,25 @@
+Les 10/05/2022  (reinstall PHP to 7.3 ~4) Dit is een test om de versie te proberen
+Commands:
+1) composer create-project symfony/skeleton:"5.4.99" symf54_10-05
+    -- Steven: composer create-project symfony/skeleton:"5.4.99" new_symf54composer
+2) composer require annotations 
+3) composer require twig
+4) composer require profiler --dev
+5) composer require debug 
+6) composer require symfony/asset 
+7) composer require maker
+
+een Command aanmaken: 
+   php bin/console make:command -> app:randompw
+
+
+composer require orm
+
+een Entity aanmaken:
+    - php bin/console make:entity 
+    - Student 
+    - naam, string, 255, verplicht
+    - voornaam, string, 255, niet verplicht 
+    - geboortedatum, date, niet verplicht
+    - punten, integer, niet verplicht
+
---
+++ symf54_10-05/src/Entity/Student.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StudentRepository::class)
+ */
+class Student
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $naam;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $voornaam;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $geboortedatum;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $punten;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNaam(): ?string
+    {
+        return $this->naam;
+    }
+
+    public function setNaam(string $naam): self
+    {
+        $this->naam = $naam;
+
+        return $this;
+    }
+
+    public function getVoornaam(): ?string
+    {
+        return $this->voornaam;
+    }
+
+    public function setVoornaam(?string $voornaam): self
+    {
+        $this->voornaam = $voornaam;
+
+        return $this;
+    }
+
+    public function getGeboortedatum(): ?\DateTimeInterface
+    {
+        return $this->geboortedatum;
+    }
+
+    public function setGeboortedatum(\DateTimeInterface $geboortedatum): self
+    {
+        $this->geboortedatum = $geboortedatum;
+
+        return $this;
+    }
+
+    public function getPunten(): ?int
+    {
+        return $this->punten;
+    }
+
+    public function setPunten(int $punten): self
+    {
+        $this->punten = $punten;
+
+        return $this;
+    }
+}
