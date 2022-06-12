384003a755d5b0f2cbd4b5b97e8a786c0e899f11
Magdalena Sarandeva <sarandeva2@gmail.com>
2022-06-12 12:35:02 +0200

    DBM configureren in config/services.yaml

--- symf54_10-05/src/Controller/HomeController.php
+++ symf54_10-05/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Services\DBManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,14 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/")
+     * @param DBManager $dbManager
      * @return Response
      */
-    public function home() {
+    public function home(DBManager $dbManager): Response
+    {
+        $data = $dbManager->GetData("select * from films");
+        dump($data);
+
         return $this->render("home/home.html.twig");
     }
 }
