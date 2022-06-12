bdd22a1fc7dd6827ef595c843d569cfae3fcc2a5
Magdalena Sarandeva <sarandeva2@gmail.com>
2022-06-12 23:34:01 +0200

    End Chapter 9:Fetching Students,_ StudentController.php

--- symf54_10-05/src/Controller/StudentController.php
+++ symf54_10-05/src/Controller/StudentController.php
@@ -13,10 +13,10 @@ class StudentController extends AbstractController
     /**
      * @Route("/student/new")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em): Response
     {
         $stu = new Student();
-        $stu->setNaam('Sarandeva')
+        $stu->setNaam('Depp')
             ->setVoornaam('Magdalena')
             ->setPunten(78);
 
@@ -29,4 +29,40 @@ class StudentController extends AbstractController
         return new Response('Nieuwe student: ' .$stu->getNaam() );
     }
 
+
+    /**
+     * @Route("/student/{id}", name="app_student_show")
+     */
+    public function show($id, EntityManagerInterface $em): Response
+    {
+        $repo = $em->getRepository(Student::class);
+
+        $student = $repo->findOneBy([ 'id' => $id ]);
+
+        // als er geen student hebben, toon error
+        if ( !$student ){
+            throw $this->createNotFoundException("Student not found");
+        }
+        dd($student);
+
+        return new Response("De gevraagde student met id $id vind je bij de dumps");
+    }
+
+    /**
+     * @Route("/students", name="app_students_show")
+     */
+    public function showAll( EntityManagerInterface $em): Response
+    {
+        $repo = $em->getRepository(Student::class);
+        $students = $repo->findAll();
+
+        // als er geen studenten hebben, toon error
+        if ( !$students ){
+            throw $this->createNotFoundException("Students not found");
+        }
+        dd($students);
+
+        return new Response("Alle students vind je bij de dumps");
+
+    }
 }
